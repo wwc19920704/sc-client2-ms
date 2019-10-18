fc5f2b36c135f660f5c455afc0e6b94960a5a773
wang.wenchao <wang.wenchao@hbfintech.com>
2019-10-18 18:48:59 +0800

    pom

---
+++ src/main/java/com/wwc/spring/cloud/client2/Client2Application.java
@@ -0,0 +1,25 @@
+package com.wwc.spring.cloud.client2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@EnableCircuitBreaker
+@EnableEurekaClient
+@EnableFeignClients
+/**
+ *	 去除加载dataSource
+ * @author wwc
+ *
+ */
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+//@SpringBootApplication
+public class Client2Application {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(Client2Application.class, args);
+	}
+}
